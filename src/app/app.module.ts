a41bf75c2a66bac2a7bff9d22db4642e699512cf
Carla Valéria <carla_empresa@outlook.com>
2017-11-21 17:09:45 -0300

    só testando algumas coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AdicionarComponent } from './adicionar/adicionar.component';
+import { GridComponent } from './grid/grid.component';
+import { PesquisarComponent } from './pesquisar/pesquisar.component';
+import { AlterarComponent } from './alterar/alterar.component';
+import { TestesComponent } from './testes/testes.component';
+import { ExcluirComponent } from './excluir/excluir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdicionarComponent,
+    GridComponent,
+    PesquisarComponent,
+    AlterarComponent,
+    TestesComponent,
+    ExcluirComponent
   ],
   imports: [
     BrowserModule,
